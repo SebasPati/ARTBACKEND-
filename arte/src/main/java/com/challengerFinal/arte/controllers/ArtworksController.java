335e2b7ff4771bd5f27323afe2d464d36d203c66
rafael <rafaninho21@gmail.com>
2022-09-13 22:05:15 -0500

    Update ArtworksController.java

--- arte/src/main/java/com/challengerFinal/arte/controllers/ArtworksController.java
+++ arte/src/main/java/com/challengerFinal/arte/controllers/ArtworksController.java
@@ -1,22 +1,30 @@
 package com.challengerFinal.arte.controllers;
 
 import com.challengerFinal.arte.dtos.ArtworksDto;
+import com.challengerFinal.arte.repositories.ArtworksRepository;
 import com.challengerFinal.arte.service.ArtworksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api")
 public class ArtworksController {
     @Autowired
-    ArtworksService service;
+    ArtworksRepository artworksService;
+
     @GetMapping(value = "/artworks")
-    public List<ArtworksDto> getArtworks() {
-        return service.getArtworks().stream().map(ArtworksDto::new).collect(Collectors.toList());
+    public Set<ArtworksDto> getArtworks() {
+        return artworksService.findAll().stream().map(ArtworksDto::new).collect(Collectors.toSet());
     }
+    @GetMapping(value = "/artworks/{id}")
+    public ArtworksDto getArtworksId(@PathVariable Long id) {
+        return artworksService.findById(id).map(ArtworksDto::new).orElse(null);
+    }
+
 }
