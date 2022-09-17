753a66cd3c38be5631d9168e8b62ec6845825b6a
rafael <rafaninho21@gmail.com>
2022-09-17 07:32:43 -0500

    rdfy

--- arte/src/main/java/com/challengerFinal/arte/repositories/ArtworksRepository.java
+++
@@ -1,7 +0,0 @@
-package com.challengerFinal.arte.repositories;
-
-import com.challengerFinal.arte.model.Artworks;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtworksRepository extends JpaRepository<Artworks, Long> {
-}
