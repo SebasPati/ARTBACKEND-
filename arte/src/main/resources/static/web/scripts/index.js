751518724b79d4e0435fa24451c32d34ccd51900
clau1069 <claudiaruiz1069@gmail.com>
2022-09-17 22:43:19 -0300

    shoppingcart.html / index.html

--- arte/src/main/resources/static/web/scripts/index.js
+++ arte/src/main/resources/static/web/scripts/index.js
@@ -1,12 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
+            tema: '',
             modal: "",
             usertype: '',
-            name: '',
-            lastName: '',
-            email: '',
-            password: '',
+            login: {
+                email: "",
+                password: ""
+            },
+            register: {
+                name: '',
+                lastName: '',
+                email: '',
+                password: ''
+            },
             repeatedPassword: ''
         }
     },
@@ -28,35 +35,22 @@ const app = Vue.createApp({
 
         /* PROPIOS DE LA PAG */
         createAccount() {
-            const request_body = {
-                "name": this.name,
-                "lastName": this.lastName,
-                "email": this.email,
-                "password": this.password
-            }
-
-            console.log(request_body)
+            console.log(this.register)
             axios.post('/api/clients', {
-                data: {
-                    "name": this.name,
-                    "lastName": this.lastName,
-                    "email": this.email,
-                    "password": this.password
-                }
-            },
-                { headers: { 'content-type': 'application/json' } })
-                .then(response => console.log("creado"))
-                .catch(error => alert(error.response.data))
-            /* console.log(this.name, this.lastName, this.email, this.password)
-            axios.post('/api/clients', {
-                "name": this.name,
-                "lastName": this.lastName,
-                "email": this.email,
-                "password": this.password
+                "name": this.register.name,
+                "lastName": this.register.lastName,
+                "password": this.register.password,
+                "email": this.register.email
             })
-                .then((response) => console.log('hecho'))
-                .catch((error) => console.log(error.response.data)) */
-
+                .then(() => console.log('created'))
+                .catch(() => console.log('error'))
+        },
+        loginAccount(e) {
+            e.preventDefault()
+            console.log(this.login)
+            axios.post('api/login', this.login)
+                .then((response) => console.log("logueado"))
+                .catch((error) => console.log("error"))
         }
     },
     computed: {
