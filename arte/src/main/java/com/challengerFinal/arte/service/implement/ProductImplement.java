f50318422ed42a034e4b0b69f2df3a7d070afe44
rafael <rafaninho21@gmail.com>
2022-09-17 19:55:57 -0500

    Crear de productos

--- arte/src/main/java/com/challengerFinal/arte/service/implement/ProductImplement.java
+++ arte/src/main/java/com/challengerFinal/arte/service/implement/ProductImplement.java
@@ -3,12 +3,15 @@ package com.challengerFinal.arte.service.implement;
 import com.challengerFinal.arte.dtos.CreateProductDto;
 import com.challengerFinal.arte.dtos.ProductDto;
 import com.challengerFinal.arte.dtos.UpdateProductDTO;
+import com.challengerFinal.arte.model.Client;
 import com.challengerFinal.arte.model.Product;
+import com.challengerFinal.arte.repositories.ClientRepository;
 import com.challengerFinal.arte.repositories.ProductRepository;
 import com.challengerFinal.arte.service.ServiceProduct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,6 +22,8 @@ import java.util.stream.Collectors;
 public class ProductImplement implements ServiceProduct {
     @Autowired
     ProductRepository productRepository;
+    @Autowired
+    ClientRepository clientRepository;
 
     @Override
     public Product saveArtworks(Product product) {
@@ -34,8 +39,26 @@ public class ProductImplement implements ServiceProduct {
     }
 
     @Override
-    public ResponseEntity<Object> createProduct(CreateProductDto createProductDto) {
-        Product newProduct =new Product ();
+    public ResponseEntity<Object> createProduct(Authentication authentication,
+                                                CreateProductDto createProductDto) {
+
+        Client client = clientRepository.findByEmail(authentication.getName());
+
+        if (
+                createProductDto.getName().isEmpty()
+                || createProductDto.getCategory().isEmpty()
+                || createProductDto.getDimensionsList().isEmpty()
+                ){
+            return new  ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        Product newProduct =new Product (
+                createProductDto.getName(),
+                createProductDto.getCategory(),
+                createProductDto.getDimensionsList(),
+                createProductDto.getDescription(),
+                client
+                );
         productRepository.save(newProduct);
         return new ResponseEntity<>("Created a new work of art"+newProduct,HttpStatus.CREATED);
     }
